5840ca660307af819629d8abfa3911feeb4dcfa3
ritikrajkvs <140308348+ritikrajkvs@users.noreply.github.com>
2025-11-13 21:39:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
-import CodingProfiles from "./components/CodingProfiles"; // if you have it
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
